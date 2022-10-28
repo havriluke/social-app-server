9622480553355f9012a7ae72b737e0b474d17ace
havriluke <havrylyuk.post@gmail.com>
2022-10-28 21:12:58 +0300

    s2

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(errorHandler)
 const server = http.createServer(app)
 const io = require('socket.io')(server, {
     cors: {
-        origin: "https://social-kreep.herokuapp.com/"
+        origin: "https://social-kreep-server.herokuapp.com/"
     }
 })
 
