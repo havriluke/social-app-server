6c428a66ef6fa9aba11ac71916979fdca8338b49
havriluke <havrylyuk.post@gmail.com>
2022-10-28 14:10:39 +0300

    b13.1

--- client/src/utils/const.js
+++ client/src/utils/const.js
@@ -8,4 +8,4 @@ export const CHAT_ROUTE = '/chat'
 export const MAIN_ROUTE = '/'
 
 console.log(process.env)
-export const REACT_APP_API_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:5000/' : 'https://social-kreep.herokuapp.com/'
+export const REACT_APP_API_URL = 'https://social-kreep.herokuapp.com/'
