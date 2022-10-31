3a1260bdbe57018f7139c504a0559cd45aee9976
havriluke <havrylyuk.post@gmail.com>
2022-10-31 22:17:02 +0200

    cors5

--- index.js
+++ index.js
@@ -12,14 +12,15 @@ const path = require('path')
 const app = express()
 
 app.use(cors({origin: true}))
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*")
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
-    next()
-})
 app.use(express.json())
 app.use(express.static(path.resolve(__dirname, 'static')))
 app.use(fileUpload({}))
+app.use((req, res, next) => {
+    res.append('Access-Control-Allow-Origin', ['*']);
+    res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.append('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+})
 app.use('/api', router)
 app.use(errorHandler)
 
