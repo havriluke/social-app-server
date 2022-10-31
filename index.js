7a68986f9148cfa3afd942ddb3bda963b1f92603
havriluke <havrylyuk.post@gmail.com>
2022-10-31 22:23:01 +0200

    cors7

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.json())
 app.use(express.static(path.resolve(__dirname, 'static')))
 app.use(fileUpload({}))
 app.use((req, res, next) => {
-    res.append('Access-Control-Allow-Origin', 'https://social-kreep.herokuapp.com/');
+    res.append('Access-Control-Allow-Origin', 'https://social-kreep.herokuapp.com');
     res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.append('Access-Control-Allow-Headers', 'Content-Type');
     next();
