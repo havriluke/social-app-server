7032e8d31c866b9e46f2b71358138bae7442cca6
havriluke <havrylyuk.post@gmail.com>
2022-11-01 17:35:04 +0200

    new db

--- db.js
+++ db.js
@@ -1,12 +1,12 @@
 const {Sequelize} = require('sequelize')
 
 module.exports = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
+    process.env.DB_NAME_HEROKU,
+    process.env.DB_USER_HEROKU,
+    process.env.DB_PASSWORD_HEROKU,
     {
         dialect: 'mysql',
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT
+        host: process.env.DB_HOST_HEROKU,
+        port: process.env.DB_PORT_HEROKU
     }
 )
